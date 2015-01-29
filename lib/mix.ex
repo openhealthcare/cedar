dbdc49015a7a94f11bce24952127a2c413c5f9ca
Ross Jones <ross@servercode.co.uk>
2015-01-29 21:53:22 +0000

    Fix the db dump and load

--- lib/mix.ex
+++ lib/mix.ex
@@ -4,7 +4,7 @@ defmodule Mix.Tasks.Cedar do
 
   defmodule Database do
 
-    @actions ["create", "dump", "delete"]
+    @actions ["create", "dump", "load", "delete"]
     @shortdoc """
     Creates or destroys the mnesia database used by CEDAR for storing
     variables.
@@ -16,28 +16,38 @@ defmodule Mix.Tasks.Cedar do
 
           mix cedar.database create
 
-          mix cedar.database dump
+          mix cedar.database dump <filename>
+
+          mix cedar.database load <filename>
 
           mix cedar.database delete
     """
     def run(_) do
-      [ _ | action ] = System.argv
-      case hd(action) in @actions do
+      #IO.inspect System.argv
+
+      [action,args] = case length(System.argv) do
+        2 -> [Enum.fetch!(System.argv, 1), []]
+        3 -> [Enum.fetch!(System.argv, 1), Enum.fetch!(System.argv, 2)]
+      end
+
+      case action in @actions do
         false -> IO.puts "Action not recognised, use create, dump or delete"
-        true ->
-          execute(hd(action))
+        true -> execute(action, args)
       end
     end
 
-    defp execute(action) do
+    defp execute(action, args) do
       case action do
-        "create" -> create
-        "dump" -> dump
-        "delete" -> delete
+        "create" -> create args
+        "dump" -> dump args
+        "load" -> load args
+        "delete" -> delete args
       end
     end
 
-    defp create do
+    """
+    """
+    defp create(_args) do
       IO.puts "Creating database ..."
 
       Amnesia.Schema.create
@@ -56,16 +66,35 @@ defmodule Mix.Tasks.Cedar do
 
     """
     """
-    defp dump do
+    defp load(filename) do
+      IO.puts "Loading the database from #{filename}"
+
       Amnesia.start
+      Db.wait
+
       Amnesia.transaction do
-        # TODO: Fix me, I am horribly broken.
-        Db.Variable |> Amnesia.Selection.values |> Enum.each &IO.inspect(&1)
+        String.to_char_list(filename) |> Amnesia.load
       end
       Amnesia.stop
     end
 
-    defp delete do
+    """
+    """
+    defp dump(filename) do
+      IO.puts "Dumping the database to #{filename}"
+
+      Amnesia.start
+      Db.wait
+
+      Amnesia.transaction do
+        String.to_char_list(filename) |> Amnesia.dump
+      end
+      Amnesia.stop
+    end
+
+    """
+    """
+    defp delete(_args) do
       IO.puts "Deleting database ..."
 
       Amnesia.start
