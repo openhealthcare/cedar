ee11f8f675b19258931c6b6b0a0e70f019ff20f7
David Miller <david@deadpansincerity.com>
2015-02-05 15:55:01 +0000

    Add an OPAL Referral action.
    This refers an episode of care for a patient to a particular team.
    We just hit the appropriate API on our OPAL endpoint.
    
    In addition, we ensure that endpoint is passed through to actions
    so that they can er, call APIs on the endpoint.
    
    refs #19

--- lib/actions/actions.ex
+++ lib/actions/actions.ex
@@ -29,32 +29,34 @@ defmodule Cedar.Actions do
         hd(Path.wildcard "#{template}/#{filename}*")
     end
 
-    def email(behaviour, [recipient, :with, content], {action, pre, post}) do
+    def email(behaviour, [recipient, :with, content], {action, pre, post, endpoint}) do
         IO.puts "email action"
         contents = EEx.eval_file template(behaviour, content), [pre: pre, post: post, episode: post]
         Cedar.Actions.Email.send resolve_var(recipient), "Auto Email from CEDAR", contents
     end
 
-    def broadcast(behaviour, [msg], {action, pre, post}) do
+    def broadcast(behaviour, [msg], {action, pre, post, endpoint}) do
         IO.puts "broadcast action"
 
         {:ok, "Sent"}
     end
 
-    def return(behaviour, [msg], {action, pre, post}) do
+    def return(behaviour, [msg], {action, pre, post, endpoint}) do
         IO.puts "return action"
         contents = EEx.eval_file template(behaviour, msg), [pre: pre, post: post]
     end
 
-    def sms(behaviour, [msg, :to, number], {action, pre, post}) do
+    def sms(behaviour, [msg, :to, number], {action, pre, post, endpoint}) do
         IO.puts "sms action"
 
         {:ok, "Sent"}
     end
 
-    def refer(behaviour, [:to, target], {action, pre, post}) do
-        IO.puts "refer"
-
+    def refer(behaviour, [:to, target], {action, pre, post, endpoint}) do
+        IO.puts "Referral action"
+        [demographics|t] = post["demographics"]
+        patient_id = demographics["patient_id"]
+        Cedar.Actions.OpalReferral.refer target, post["id"], patient_id, endpoint
         {:ok, "Sent"}
     end
 
