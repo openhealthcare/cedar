342cfcf44275b4baccb0b14bd7be55ec2bf17420
Ross Jones <ross@servercode.co.uk>
2015-02-09 16:39:14 +0000

    Minor tweaks to trick travis into running

--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :cedar, Cedar.Endpoint,
-  http: [port: System.get_env("PORT") || 4001]
-
+  http: [port: System.get_env("PORT") || 4001],
+  external_actions: false
 
 config :logger, level: :warn
 
