6e590cea9b723e5d3183e7b658623b19439184d6
Ross Jones <ross@servercode.co.uk>
2015-04-14 17:07:00 +0100

    Partially unbreak variable resolution

--- lib/vars.ex
+++ lib/vars.ex
@@ -1,8 +1,21 @@
 defmodule Cedar.Vars do
+  alias Cedar.Variable
 
+  def resolve_var("@" <> key) do
+    variable = Repo.get(Variable, key)
+    resolve_result(variable.value, key)
+  end
 
   def resolve_var(key) do
     key
+  end
+
+  defp resolve_result(_, deflt) do
+    deflt
+  end
+
+  defp resolve_result(val, _deflt) do
+    val
   end
 
 end
