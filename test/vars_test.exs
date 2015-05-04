b5f20791dbb0a38e838abf9eba940537c3f54c3e
Ross Jones <ross@servercode.co.uk>
2015-05-04 16:49:26 +0100

    Fix tests for VarServer

--- test/vars_test.exs
+++ test/vars_test.exs
@@ -3,12 +3,13 @@ defmodule VariablesTest do
   alias Cedar.VarServer, as: Vars
 
   test "Create a new variable " do
-    Vars.create("Test var", "Test value")    
+    Vars.create("Test var", "Test value")
     assert Vars.lookup("Test var") == "Test value"
+    Vars.delete("Test var")
   end
 
-  test "We can get all variables" do 
-    assert Dict.size(Vars.all) == 1
-  end 
+  test "We can get all variables" do
+    assert Dict.size(Vars.all) == 0
+  end
 
 end
