6fc3290370dd252c19f7f2b32c8cf03ec8d5039a
Ross Jones <ross@servercode.co.uk>
2015-05-11 20:11:35 +0100

    Trying explicit channel name

--- priv/static/js/audit.js
+++ priv/static/js/audit.js
@@ -1,10 +1,10 @@
 
 var $messages = $("#messages");
 var socket = new Phoenix.Socket("/ws")
-var chan = socket.chan("audit", {});
+var chan = socket.chan("audit:all", {});
 console.log("Connected to channel");
 
-chan.on("new:message", function(msg){
+chan.on("*", function(msg){
     console.log(msg)
     $messages.append("<br/>" + msg.id + " ->"  + msg.success + " - " + msg.behaviour)
 });
