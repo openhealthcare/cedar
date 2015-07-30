318350af086c9e8e36a7db5dde174c653064df68
rossjones <ross@servercode.co.uk>
2015-07-30 14:44:52 +0100

    Fix audit stream for yet more channel changes

--- priv/static/js/audit.js
+++ priv/static/js/audit.js
@@ -2,12 +2,20 @@
 var $messages = $("#messages");
 
 
-var socket = new Phoenix.Socket("/ws")
-socket.connect()
-socket.join("audit:all").receive("ok", function(chan){
-    console.log("Connected to WS")
-    chan.on("new:message", function(msg){
-        console.log(msg)
-        $messages.append("<br/>" + msg.id + " ->"  + msg.success + " - " + msg.behaviour)
-    });
-})
+var socket = new Phoenix.Socket("/ws");
+socket.connect();
+socket.onClose( function(e) {console.log("CLOSE", e)})
+
+var chan = socket.chan("audit:all", {});
+
+chan.on("new:message", function(msg){
+    console.log(msg);
+    $messages.append("<br/>" + msg.id + " ->"  + msg.success + " - " + msg.behaviour);
+});
+
+chan.join().receive("ok", function(c){
+    console.log("Connected to WS");
+});
+
+
+
